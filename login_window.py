0823d7c7f819428420822192ea7d1bf186e46e23
Isabella Dos Anjos Scarassati Celici <isabellascarassatii@gmail.com>
2025-11-30 21:12:40 -0300

    docs: update login_window.py

--- login_window.py
+++ login_window.py
@@ -11,18 +11,16 @@ class LoginWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle("Login - Belle Time")
         
         self.botao_entrar.clicked.connect(self.tentar_login)
-        # Permite apertar ENTER no campo de senha para entrar
         self.campo_senha.returnPressed.connect(self.tentar_login)
         
-        self.login_sucesso = False # Variável de controle
+        self.login_sucesso = False 
 
     def tentar_login(self):
-        # AQUI É O ERRO: SEMPRE LIMPE O INPUT ANTES DE ENVIAR
         usuario = self.campo_usuario.text().strip()
         senha = self.campo_senha.text().strip()
         
         if database.verificar_login(usuario, senha):
             self.login_sucesso = True
-            self.close() # ...
+            self.close() 
         else:
             QMessageBox.warning(self, "Erro", "Login incorreto!")
